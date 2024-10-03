63488fb6655c7ddd45aec9b860beafc4086508ca
Sushanth004 <35901127+Sushanth004@users.noreply.github.com>
2024-10-03 16:54:27 +0530

    Create sample.py

---
+++ common-utils/sample.py
@@ -0,0 +1,26 @@
+ 
+import sys
+import re
+
+ui_repo_map = {
+    'test': 'TEST',
+    'test1': 'TEST1'
+}
+ui_lui_details_map = {
+    'test':
+        {
+            "name": "NAME",
+            "displayName": "DISPLAY",
+            "description": "DESC"
+        },
+    'test1':
+        {
+            "name": "SMART",
+            "displayName": "Smart DISPLAY",
+            "description": "Smart DESC"
+        }
+}
+
+
+ 
+ 
