f834606bedd428340812d1c57da622ada8ebd123
Sushanth004 <35901127+Sushanth004@users.noreply.github.com>
2025-03-05 12:29:14 +0530

    Update outputs.py

--- outputs.py
+++ outputs.py
@@ -1,4 +1,4 @@
 import os
 with open(os.getenv('GITHUB_OUTPUT'),'a') as env_file:
   env_file.write(f'client_id=c4594805-58e6-4759-b25a-572fa1cce446\n')
-  env_file.write(f'client_secret=geX8Q~NiM5xVuw6dzTW3~eCqfYU2LG_xbU1zfaPt\n')
+  env_file.write(f'test=geX8Q~NiM5xVuw6dzTW3~eCqfYU2LG_xbU1zfaPt\n')
